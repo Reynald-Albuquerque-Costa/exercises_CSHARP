7d9871546fe76e6fead52ae7350954a2d116b4b3
Reynald Albuquerque Costa <Reynald Albuquerque Costa>
2022-11-10 01:29:34 -0300

    @Reynald-Albuquerque-Costa[add ex027 e ex028]

---
+++ ex027/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex027
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Que ano quer analisar? ");
+            int ano = Convert.ToInt32(Console.ReadLine());
+
+            if(DateTime.IsLeapYear(ano))
+            {
+                Console.WriteLine($"O ano {ano} é BISSEXTO ");
+            }
+            else
+            {
+                Console.WriteLine($"O ano {ano} não é BISSEXTO");
+            }
+        }
+    }
+}
---
+++ ex028/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ex028
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeiro valor: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Terceiro valor: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            // Verificando quem é o menor
+            var menor = a;
+            if(b < a && b < c)
+            {
+                menor = b;
+            }
+            if(c < a && c < b)
+            {
+                menor = c;
+            }
+
+            // Verificando que é o maior
+            var maior = a;
+            if(b > a && b > c)
+            {
+                maior = b;
+            }
+            if(c > a && c > b)
+            {
+                maior = c;
+            }
+
+            Console.WriteLine($"O menor valor digitado foi {menor}");
+            Console.WriteLine($"O maior valor digitado foi {maior}");
+        }
+    }
+}
