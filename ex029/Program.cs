b45b4729fe17d5aa3d46e42d3d239310beec77f2
Reynald Albuquerque Costa <Reynald Albuquerque Costa>
2022-11-15 23:08:38 -0300

    @Reynald-Albuquerque-Costa[add ex029 e ex030]

---
+++ ex029/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex029
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal novoSalario;
+
+            Console.WriteLine("Qual é o salário do funcionário? R$ ");
+            decimal salario = Convert.ToDecimal(Console.ReadLine());
+
+            if(salario <= 1250)
+            {
+                novoSalario = salario + (salario * 15 / 100);
+            }
+            else
+            {
+                novoSalario = salario + (salario * 10 / 100);
+            }
+
+            Console.WriteLine($"Quem ganhava R$ {salario} passa a ganhar R$ {novoSalario} agora ");
+        }
+    }
+}
---
+++ ex030/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex030
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primeiro segmento: ");
+            double r1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Segundo segmento: ");
+            double r2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Terceiro segmento: ");
+            double r3 = Convert.ToDouble(Console.ReadLine());
+
+            if(r1 < r2 + r3 && r2 < r1 + r3 && r3 < r1 + r2)
+            {
+                Console.WriteLine("Os segmentos acima PODEM FORMAR um triângulo!");
+            }
+            else
+            {
+                Console.WriteLine("Os segmentos acima NÃO PODEM FORMAR um triângulo");
+            }
+        }
+    }
+}
