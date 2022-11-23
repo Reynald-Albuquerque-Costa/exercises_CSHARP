97f0c19c876c9528b6333642f206e1ba794f3d59
Reynald Albuquerque Costa <Reynald Albuquerque Costa>
2022-11-23 00:24:46 -0300

    @Reynald-Albuquerque-Costa[add ex036 e ex037]

---
+++ ex036/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ex036
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite seu peso: ");
+            float peso = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite sua altura: ");
+            float altura = float.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Seu peso é " + peso);
+            Console.WriteLine("Sua altura é " + altura);
+
+
+            var imc = peso / (Math.Pow(altura, 2));
+
+            if (imc < 18.5)
+                Console.WriteLine("Abaixo do peso");
+            else if (imc > 18.5 && imc < 25)
+                Console.WriteLine("Peso ideal");
+            else if (imc <= 30)
+                Console.WriteLine("Sobrepeso");
+            else if (imc > 30 && imc < 40)
+                Console.WriteLine("Obesidade");
+            else
+                Console.WriteLine("Obesidade mórbida");
+        }
+    }
+}
---
+++ ex037/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ex037
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var total = 0;
+            var parcela = 0;
+
+            Console.WriteLine("Preço das compras: R$ ");
+            int preco = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("FORMAS DE PAGAMENTO \r\n " +
+
+                "[ 1 ] á vista dinheiro/cheque \r\n" +
+                "[ 2 ] á vista cartão \r\n" +
+                "[ 3 ] 2x no cartão \r\n" +
+                "[ 4 ] 3x ou mais no cartão ");
+
+
+            Console.WriteLine("Qual é a opção? ");
+            int opcao = int.Parse(Console.ReadLine());
+
+            if (opcao == 1)
+            {
+                total = preco - (preco * 10 / 100);
+            }
+            else if (opcao == 2)
+            {
+                total = preco - (preco * 5 / 100);
+            }
+            else if (opcao == 3)
+            {
+                total = preco;
+                parcela = preco / 2;
+                Console.WriteLine($"Sua compra será parcelada em 2x de {parcela.ToString("F2")} SEM JUROS ");
+            }
+            else if (opcao == 4)
+            {
+                total = preco + (preco * 20 / 100);
+
+                Console.WriteLine("Quantas parcelas ");
+                int totalParcela = int.Parse(Console.ReadLine());
+
+                parcela = total / totalParcela;
+                Console.WriteLine($"Sua compra de R$ {preco.ToString("F2")} será parcelada em {totalParcela}x de {parcela.ToString("F2")} COM 20% de JUROS");
+            }
+
+
+            Console.WriteLine($"Sua compra de R$ {preco.ToString("F2")} vai custar R$ {total.ToString("F2")} no final ");
+        }
+    }
+}
