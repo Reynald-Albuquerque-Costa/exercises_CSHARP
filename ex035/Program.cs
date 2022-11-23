9b01678b263898d7eee038e9128d26e65ef8e249
Reynald Albuquerque Costa <Reynald Albuquerque Costa>
2022-11-22 22:55:03 -0300

    @Reynald-Albuquerque-Costa[add ex035]

---
+++ ex035/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex035
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoAtual = DateTime.Today.Year;
+
+            Console.WriteLine("Ano de nascimento: ");
+            int anoNascimento = int.Parse(Console.ReadLine());
+
+            int idade = anoAtual - anoNascimento;
+
+            Console.WriteLine($"O atleta tem {idade} anos ");
+
+            if (idade <= 9)
+                Console.WriteLine("Classificação: MIRIM");
+            else if (idade <= 14)
+                Console.WriteLine("Classificação: INFANTIL");
+            else if (idade <= 19)
+                Console.WriteLine("Classificação: JUNIOR");
+            else if (idade <= 25)
+                Console.WriteLine("Classificação: SÊNIOR");
+            else
+                Console.WriteLine("Classificação: MASTER");
+        }
+    }
+}
