9807ed699841692dda3a1b861846a9d1e0b79e06
Reynald Albuquerque Costa <Reynald Albuquerque Costa>
2022-11-24 00:26:21 -0300

    @Reynald-Albuquerque-Costa[add ex039]

---
+++ ex039/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace ex039
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Contagem regressiva 
+
+            for (int x = 0; x <= 10; x++)
+            {
+                var total = 10 - x;
+                Console.WriteLine(total);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
