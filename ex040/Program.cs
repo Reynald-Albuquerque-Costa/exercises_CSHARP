3f316ec9f5269316427f95c180cc1866a3e1ff48
Reynald Albuquerque Costa <Reynald Albuquerque Costa>
2022-12-16 20:04:32 -0300

    add ex040 e ex041

---
+++ ex040/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex040
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int x = 0; x <= 50 ; x++)
+            {
+                if(x % 2 == 0)
+                    Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ ex041/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex041
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Soma entre todos os números impares que são múltiplos de três e que se encontram em um intervalo de 1 a 500
+
+            int soma = 0;
+
+            for(int x = 1; x <= 500; x++)
+            {
+                if(x % 2 != 0)
+                    if(x % 3 == 0)
+                        soma += x;
+            }
+
+            Console.WriteLine($"A soma de todos os valores solicitados é {soma}");
+        }
+    }
+}
